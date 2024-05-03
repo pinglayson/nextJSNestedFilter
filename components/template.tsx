3718b94c5c8dbba4a38b5b80009a05f077587e3f
Ping Layson <pinglayson@users.noreply.github.com>
2024-05-03 14:28:45 +1000

    feat(ping): exercise

--- components/template.tsx
+++ components/template.tsx
@@ -1,16 +1,56 @@
 import { FunctionComponent } from "react";
 import { Car, PageData } from "../interfaces";
+import { parse } from "node-html-parser";
+// import { DOMParser}
 
-export const PageTemplate:FunctionComponent<PageTemplateProp> = ({pageInfo, cars}) => {
+export const PageTemplate: FunctionComponent<PageTemplateProp> = ({
+  pageInfo,
+  cars,
+}) => {
   // get cars and pageInfo from ssr and also refetch from the client side too as data may change quickly
+
+  const carFiltered = cars.filter((car) =>
+    car.families.some((family) => family.baseVariantImages.length > 0)
+  );
+
+  function htmlDecode(input) {
+    let doc = parse(input);
+    return doc.textContent;
+  }
+
+  function getImage(car) {
+    let families = car.families;
+    let familyWithImage = families.find(
+      (family) => family.baseVariantImages.length > 0
+    );
+
+    return familyWithImage.baseVariantImages[0];
+  }
 
   return (
     <div>
       <h1>Cars List</h1>
-      <div className='page-info'>
+
+      <div className="page-info">
         {/* create a component that displays the page info */}
+        <div style={{ display: "flex" }}>
+          <h2>{pageInfo.title}</h2>
+        </div>
+        <div style={{ display: "flex" }}>
+          <div>{htmlDecode(pageInfo.excerpt)}</div>
+        </div>
+        <div>{pageInfo.content}</div>
       </div>
-      <div className='car-cards-container'>
+      <div
+        className="car-cards-container"
+        style={{ display: "grid", gridTemplateColumns: "auto auto auto" }}
+      >
+        {carFiltered.map((car) => (
+          <div key={car.uuid}>
+            <p>{car.title}</p>
+            <img src={getImage(car)} style={{ height: "100px" }} />
+          </div>
+        ))}
         {/* create a component that displays list of cars
         Only display cars by make name that has atleast one family image.
         Each make cars families must be ordered by image first priority. 
@@ -18,11 +58,10 @@ export const PageTemplate:FunctionComponent<PageTemplateProp> = ({pageInfo, cars
         */}
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 type PageTemplateProp = {
-  pageInfo?: PageData,
-  cars?: Car[]
-}
+  pageInfo?: PageData;
+  cars?: Car[];
+};
--- pages/cars/index.tsx
+++ pages/cars/index.tsx
@@ -1,36 +1,39 @@
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
-import { Car, PageData } from '../../interfaces'
-import Layout from '../../components/Layout'
-import { PageTemplate } from '../../components/template'
+import { Car, PageData } from "../../interfaces";
+import Layout from "../../components/Layout";
+import { PageTemplate } from "../../components/template";
 
 type Props = {
-  cars: Car[]
-  pageInfo: PageData
-}
+  cars: Car[];
+  pageInfo: PageData;
+};
 
 const WithServerSideProps = ({ cars, pageInfo }: Props) => {
-  console.log({cars, pageInfo})
-  
+  console.log({ cars, pageInfo });
+
   return (
-
     <Layout title="Users List | Next.js + TypeScript Example">
-      <PageTemplate cars={cars} pageInfo={pageInfo}/>
+      <PageTemplate cars={cars} pageInfo={pageInfo} />
     </Layout>
-  )
-}
+  );
+};
 
-export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
-
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
   // Example for including serverSide props in a Next.js function component page.
   // Don't forget to include the respective types for any props passed into
   // the component.
 
   // Fetch pageInfo and Cars Data via api and pass that to client
 
-  const cars = []
-  const page = {}
-  return { props: { cars, pageInfo: page } }
-}
+  const response = await fetch("http://localhost:3000/api/cars");
+  const cars = await response.json();
 
-export default WithServerSideProps
+  const responsePage = await fetch("http://localhost:3000/api/page");
+  const page = await responsePage.json();
+  return { props: { cars, pageInfo: page } };
+};
+
+export default WithServerSideProps;
